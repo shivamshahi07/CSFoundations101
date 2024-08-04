5d00a2c04f562359e8c5979743cb7795e1a80ef7
Shivam Shahi <shivam.shahi06@gmail.com>
2024-08-05 01:30:07 +0530

    added cms

--- csf/app/(landing)/notes/page.tsx
+++ csf/app/(landing)/notes/page.tsx
@@ -1,9 +1,34 @@
 import Image from "next/image";
+import {client} from "../../../lib/contentful"; 
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-export default function Home() {
+
+export default async function Home() {
+
+  const notesresponse = await client.getEntries({
+    content_type: "notesApp",
+  });
+  const notes=notesresponse.items.map((item: any) => ({
+    topic: item.fields.topic,
+    content: item.fields.content,
+    tags: item.fields.tags,
+    createdDate: item.fields.createdDate,
+    updatedDate: item.fields.updatedDate,
+  })); 
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello from notes page
+      <h1 className="text-6xl font-bold">Notes</h1>
+      <div className="flex flex-col gap-12">
+        <p>{notes[0].topic}</p>
+        <div>{documentToReactComponents(notes[0].content)}</div>
+        {/* <p>{notes[0].tags}</p>
+        <p>{notes[0].createdDate}</p>
+        <p>{notes[0].updatedDate}</p>
+         */}
+        
+      </div>
+      
     </main>
   );
 }
---
+++ csf/lib/contentful.ts
@@ -0,0 +1,6 @@
+import * as contentful from 'contentful';
+
+export const client = contentful.createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID!,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN!,
+});
